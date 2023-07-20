8bb53da423e1fc811a9ce2aa1edfbf7ef65fa5e9
Angelika Barnowska-Nawara <angelika.barnowska@gmail.com>
2023-07-20 21:49:59 +0200

    fix navigation and scrollspy -succes

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,59 @@
-const nav = document.querySelector(".nav-mobile__items");
-const navMain = document.querySelector(".nav-mobile__main");
-const burgerBtn = document.querySelector(".burger-btn");
+"use strict";
+const menu = document.querySelector(".navbar__list");
+const allNavItems = document.querySelectorAll(".navbar__item a");
+const burgerBtn = document.querySelector(".navbar__burger-btn");
 const logo = document.querySelector(".fa-solid");
-const allNavLinks = document.querySelectorAll(".nav-mobile__item");
+const allSections = document.querySelectorAll(".section");
+
 const footerYear = document.querySelector(".footer__year");
-const allDesktopItems = document.querySelectorAll(".nav-desktop__item");
-const scrollSpySections = document.querySelectorAll(".section");
 
 const showNav = () => {
-	nav.classList.toggle("nav-mobile--active");
-	if (nav.classList.contains("nav-mobile--active")) {
+	menu.classList.toggle("show-menu");
+
+	if (menu.classList.contains("show-menu")) {
 		burgerBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
 		burgerBtn.style.color = "rgb(2, 70, 2)";
-		navMain.classList.remove("nav-mobile__main--active");
 	} else {
 		burgerBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
-		burgerBtn.style.color = "#fff";
-		showMainNav();
 	}
 };
 
-allNavLinks.forEach(item =>
+allNavItems.forEach(item =>
 	item.addEventListener("click", () => {
-		nav.classList.remove("nav-mobile--active");
+		menu.classList.remove("show-menu");
 		burgerBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
-		burgerBtn.style.color = "#fff";
 	})
 );
-
-const showMainNav = () => {
-	if (window.scrollY > 200) {
-		navMain.classList.add("nav-mobile__main--active");
-	} else {
-		navMain.classList.remove("nav-mobile__main--active");
-	}
-};
-
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.textContent = year;
-};
 
 const handleScrollSpy = () => {
 	if (document.body.classList.contains("main-page")) {
 		const sections = [];
 
-		scrollSpySections.forEach(section => {
-			if (window.scrollY <= section.offsetTop + section.offsetHeight - 70) {
+		allSections.forEach(section => {
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 80) {
 				sections.push(section);
 
 				const activeSection = document.querySelector(
 					`[href*="${sections[0].id}"]`
 				);
 
-				allDesktopItems.forEach(item =>
-					item.classList.remove("scrollspy-active")
+				allNavItems.forEach(item =>
+					item.classList.remove("navbar__item--active")
 				);
 
-				activeSection.classList.add("scrollspy-active");
+				activeSection.classList.add("navbar__item--active");
 			}
 		});
 	}
 };
 
-if (!document.body.classList.contains("main-page")) {
-	allDesktopItems.forEach(el => {
-		el.classList.remove("scrollspy-active");
-	});
-}
+console.log(allNavItems[0].parentElement);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.textContent = year;
+};
 
 burgerBtn.addEventListener("click", showNav);
-window.addEventListener("scroll", showMainNav);
-handleCurrentYear();
 window.addEventListener("scroll", handleScrollSpy);
+handleCurrentYear();
